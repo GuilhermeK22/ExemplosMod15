906bb2b7b6b659d7753e884c0d00ce9c7009ed79
Guilherme <guilhermekappaun@gmail.com>
2023-09-02 18:53:38 -0300

    Exemplo

--- src/br/com/gui/Teste.java
+++ src/br/com/gui/Teste.java
@@ -1,2 +1,18 @@
-package br.com.gui;public class Teste {
+package br.com.gui;
+
+public class Teste {
+    public static void main(String[] args) {
+        Cliente cliente1 = new Cliente("A", false);
+        Fabrica fabrica = getSemContratosFabrica(cliente1);
+        Carro carro1 = fabrica.criar(cliente1.getPedido());
+        carro1.ligarMotor();
+
+    }
+    private static Fabrica getSemContratosFabrica(Cliente cliente) {
+        if (cliente.isContratoEmpresa()) {
+            return new ContratosFabrica();
+        } else {
+            return new SemContratosFabrica();
+        }
+    }
 }
